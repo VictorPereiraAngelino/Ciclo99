2a7a7964179a3308f332037678f1f9e5add87768
Victor Pereira Angelino <victorpereiraangelino6@gmail.com>
2024-10-30 15:57:34 -0600

    Ciclo 100

---
+++ main.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int atual;
+    int ano;
+    int idade;
+    cout << "Ano De Nascimento : ";
+    cin >> ano;
+    cout << "Ano De Atual: ";
+    cin >> atual;
+     switch (ano) {
+    case 1996:
+    break;
+     }
+     switch (atual) {
+    case 1996:
+    break;
+    case 1997:
+    break;
+    case 1998:
+    break;
+    case 1999:
+    break;
+    case 2000:
+    break;
+    case 2001:
+    break;
+     case 2002:
+    break;
+     case 2003:
+    break;
+     case 2004:
+    break;
+     case 2005:
+    break;
+     case 2006:
+    break;
+     case 2007:
+    break;
+     case 2008:
+    break;
+     case 2009:
+    break;
+     case 2010:
+    break;
+    case 2011:
+    break;
+    case 2012:
+    break;
+    case 2013:
+    break;
+    case 2014:
+    break;
+    case 2015:
+    break;
+    case 2016:
+    break;
+    case 2017:
+    break;
+    case 2018:
+    break;
+    case 2019:
+    break;
+    case 2020:
+    break;
+    case 2021:
+    break;
+    case 2022:
+    break;
+    case 2023:
+    break;
+    case 2024:
+    break;
+    case 2025:
+    break;
+    case 2026:
+    break;
+    case 2027:
+    break;
+    case 2028:
+    break;
+    case 2029:
+    break;
+    case 2030:
+    break;
+    case 2031:
+    break;
+    case 2032:
+    break;
+    case 2033:
+    break;
+    case 2034:
+    break;
+    case 2035:
+    break;
+    case 2036:
+    break;
+    case 2037:
+    break;
+    case 2038:
+    break;
+    case 2039:
+    break;
+    case 2040:
+    break;
+    case 2041:
+    break;
+    case 2042:
+    break;
+    case 2043:
+    break;
+    case 2044:
+    break;
+    case 2045:
+    break;
+    case 2046:
+    break;
+    case 2047:
+    break;
+    case 2048:
+    break;
+    case 2049:
+    break;
+    case 2050:
+    break;
+    case 2051:
+    break;
+    case 2052:
+    break;
+    case 2053:
+    break;
+    case 2054:
+    break;
+    case 2055:
+    break;
+    case 2056:
+    break;
+    case 2057:
+    break;
+    case 2058:
+    break;
+    case 2059:
+    break;
+    case 2060:
+    break;
+    case 2061:
+    break;
+    case 2062:
+    break;
+    case 2063:
+    break;
+    case 2064:
+    break;
+    case 2065:
+    break;
+    case 2066:
+    break;
+    case 2067:
+    break;
+    case 2068:
+    break;
+    case 2069:
+    break;
+    case 2070:
+    break;
+    case 2071:
+    break;
+    case 2072:
+    break;
+    case 2073:
+    break;
+    case 2074:
+    break;
+    case 2075:
+    break;
+    case 2076:
+    break;
+    case 2077:
+    break;
+    case 2078:
+    break;
+    case 2079:
+    break;
+    case 2080:
+    break;
+    case 2081:
+    break;
+    case 2082:
+    break;
+    case 2083:
+    break;
+    case 2084:
+    break;
+    case 2085:
+    break;
+    case 2086:
+    break;
+    case 2087:
+    break;
+    case 2088:
+    break;
+    case 2089:
+    break;
+    case 2090:
+    break;
+    case 2091:
+    break;
+    case 2092:
+    break;
+    case 2093:
+    break;
+    case 2094:
+    break;
+    case 2095:
+    break;
+    case 2096:
+    break;
+    case 2097:
+    break;
+    case 2098:
+    break;
+    case 2099:
+    break;
+
+     }
+    idade = ano - atual;
+
+    cout << "Idade atual : "<< idade << " Em : "<< atual;
+
+}
